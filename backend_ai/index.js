e3560bd87f3e97aef630a950b378e2eb9258a54f
Saumya Gurnani <saumyagurnani73@gmail.com>
2025-11-05 13:54:54 +0530

    backend connection

--- backend_ai/index.js
+++ backend_ai/index.js
@@ -2,11 +2,9 @@ const express=require('express')
 const app=express();
 const PORT=4000;
 require('./conn');
-app.get('/', (req,res)=>{
-    res.send({
-            message:"Hi Welcome bye "
-        } )
-})
+app.use(express.json());
+const UserRoutes=require('./Routes/user');
+app.use('/api/user', UserRoutes)
 app.listen(PORT, ()=>{
     console.log("backend is running on port", PORT)
 })
