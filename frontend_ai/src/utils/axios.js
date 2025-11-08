a7182ab11016bd48c9aa34e76bd372348e3329e9
Saumya Gurnani <saumyagurnani73@gmail.com>
2025-11-08 23:51:48 +0530

    connection

--- frontend_ai/src/utils/axios.js
+++ frontend_ai/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance= axios.create({
-    baseURL: '/',
+    baseURL: 'https://resume-analyzer-backend-1nl2.onrender.com',
     
 })
 export default instance;
